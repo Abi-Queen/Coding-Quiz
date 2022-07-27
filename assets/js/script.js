a574e5579f67544773926d2d1f54e479bd5a4384
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-07-26 22:38:12 -0700

    comment out js, outline html

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+//create an array of questions and answers; first question is the coding quiz greeting; last question is initials/score
+
+//create a for loop to cycle through the questions
+
+//add conditionals to keep wrong questions open but move past correct questions 
+
+//add conditionals to display "correct" or "wrong" feedback
+
+//set the question for loop to display in the div container
+
+//add a timer penalty
+
+//add a score-keeper
+
+//display score at end of loop
+
+//prompt user to enter initials to save score
+
+//add form for user to enter initials
+
+//add table to display high scores 
+
+//add button to retake quiz
