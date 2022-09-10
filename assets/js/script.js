d74181b6266fcb52ca7830e98105717e4a3d4b90
Abigail Queen <107533064+Abi-Queen@users.noreply.github.com>
2022-09-10 07:01:21 -0700

    cleaning up startquiz funtion

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,118 +52,13 @@ var questions = [{
 
 // Quiz functionality 1: start
 //startbtn startQuiz
-startQuiz = () => {
-    questionCounter = 0;
-    var question = getNewQuestion(questions,0);
-    displayQuestion(question);
-}
+$(".startBtn").on("click", function(){
+    startQuiz = () => {
+        questionCounter = 0;
+        var question = getNewQuestion(questions,0);
 
-// Quiz functionality 2: display questions and options
-displayQuestion = (question) => {
-    $('#question').innerText=question.text
-    document.getElementById('display-question').innerText = question.question
-    document.querySelector(".option-container:nth-child(1)").innerText = question.option1
-    document.querySelector(".option-container:nth-child(2)").innerText = question.option2
-    document.querySelector(".option-container:nth-child(3)").innerText = question.option3
-    document.querySelector(".option-container:nth-child(4)").innerText = question.option4
-    // answer: 4
-    // option1: "The <head> section"
-    // option2: "Both the <head> section and the <body> section"
-    // option3: "The beginning of the <body> section"
-    // option4: "The end of the <body> section"
-    // question: "Where is the correct place to insert a JavaScript?"
-    // div:nth-child(2)
-}
-
-getNewQuestion = (questions, position) => {
-    var question = questions[position]
-    return question;
-//     console.log(availableQuestions)
-//     if(availableQuestions.length === 0 || questionCounter > maxQuestions){
-//         // display end page
-//         //return window.location.assign("/end.html"); 
-//     }
-//     questionCounter++; 
-//     var questionIndex = Math.floor(Math.random() + availableQuestions.length);
-//     currentQuestion = availableQuestions[questionIndex];
-//     question.innerText = currentQuestion.question;
-
-//     options.forEach(option => {
-//         const number = option.dataset['number'];
-//         option.innerText = currentQuestion['option' + number];
-//     });
-//     //omit question we just used from current array
-//     availableQuestions.splice(questionIndex, 1);
-};
-
-//check answers to see if they are correct by inspecting element of clicked option
-// options.forEach(option => {
-//     option.addEventListener('click', e => {
-//         const selectedOption = e.target;
-//         const selectedAnswer = selectedOption.dataset['number'];
-
-//         const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
-
-//         //add points to score for correct answer
-//         if (classToApply === "correct") {
-//             incrementScore(correctPoints);
-//         }
-
-//     selectedOption.parentElement.classList.add(classToApply);
-
-//     setTimeout( () => {
-//         selectedOption.parentElement.classList.remove(classToApply);
-//         getNewQuestion();
-//     }, 1000);
-//     });
-// });
-
-document.querySelector(".options-container").onclick = (event) => {
-    var clickedElement = event.target;
-    if(clickedElement.matches('.option-container') && clickedElement.getAttribute("data-option")=="4")
-    /** TODO. Change "1" to question.answer */
-        alert("Correct!")
-    else
-        alert("Incorrect!")
-}
-
-//keep score
-incrementScore = num => {
-    score +=num;
-    scoreText.innerText = score;
-};
-
-
-startQuiz();
-
-
-//timer start
-function timerStart() {
-    time = setInterval(() => {timer(); }, 1000);
-}
-
-document.getElementbyId('#start-quiz-btn').onclick = timerStart();
-
-//timer reset
-function timerReset() {
-    minute = 0;
-    second = 0;
-    document.getElementById('minute').innerText = '00';
-    document.getElementById('second').innerText = '00';
-}; 
-
-// timer functionality 
-function timer() {
-    if (second == 60) {
-        second = 0;
-        minute ++;
-    }
-    if (minute == 60) {
-        minute = 0;
-        hour ++; 
-    }
-    document.getElementById('hour').innerText = returnData(hour);
-    document.getElementById('minute').innerText = returnData(minute);
-    document.getElementById('second').innerText = returnData(second);
-}; 
-
+        // Quiz functionality 2: display questions and options
+        displayQuestion = (question) => {
+        $('#question').innerText=question.text
+    
+    
